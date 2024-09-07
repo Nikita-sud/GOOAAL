321fde4a01aba579016a4fd73c11cab6074adb2f
Nikita <bulgarunichita0@gmail.com>
2024-09-07 22:00:37 +0200

    Подключил свою базу для себя

--- src/backend/database.py
+++ src/backend/database.py
@@ -2,9 +2,9 @@ import mysql.connector
 
 def connect_to_db():
     connection = mysql.connector.connect(
-        host="localhost",        # Хост вашего сервера MySQL
-        user="your_user",        # Ваш пользователь MySQL
-        password="your_password",# Пароль пользователя MySQL
-        database="pizza_shop"    # Ваша база данных
+        host="localhost",
+        user="Nichita",
+        password="28082004", 
+        database="pizza_shop"
     )
     return connection
--- src/backend/models.py
+++ src/backend/models.py
@@ -3,7 +3,7 @@ from src.backend.database import connect_to_db
 def add_order(customer_id, pizza_id, status="processing"):
     connection = connect_to_db()
     cursor = connection.cursor()
-    query = "INSERT INTO orders (customer_id, pizza_id, status) VALUES (%s, %s, %s)"
+    query = "INSERT INTO orders (customer_id, pizza_id, status1) VALUES (%s, %s, %s)"
     cursor.execute(query, (customer_id, pizza_id, status))
     connection.commit()
     cursor.close()

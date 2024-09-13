858aced4b78f742abd1767ee25acee094befafa2
t3i8m <number.38.tj@gmail.com>
2024-09-14 01:24:46 +0200

    refactored main.py

--- src/ui/mobile_main.py
+++ src/ui/mobile_main.py
@@ -1,12 +1,13 @@
 from kivy.app import App
 from kivy.lang import Builder 
-from kivy.uix.screenmanager import ScreenManager 
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
-
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from kivy.clock import Clock
 
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from src.backend.repositories.customers.customer_intfc import CustomerInterface
 from src.backend.repositories.customers.customer_repo import CustomerRepo
 from src.backend.models import add_order
@@ -14,50 +15,73 @@ from src.backend.database import connect_to_db
 
 
 
+
+class PizzaScreenManager(ScreenManager):
+    pass
+class LoginScreen(Screen):
+    def login(self):
+        app = App.get_running_app()
+        username = self.ids.username_input.text
+        password = self.ids.password_input.text
+
+        if not username or not password:
+            self.ids.error_label.text = "Username and password cannot be empty!"
+            # return
+        else:
+
+            try:
+                connection = connect_to_db()
+                customer_repo: CustomerInterface = CustomerRepo(connection)
+
+
+                customer_repo.create_user(username, "", 1, "08.12.2004", 2, 1)
+
+                app.username = username
+                app.password = password
+                app.connection = connection  
+                self.manager.current = 'loading_screen'
+                self.manager.current = 'order_screen'
+            except Exception as e:
+                print(f"Ошибка входа: {e}")
+                print("Неверное имя пользователя или пароль. Пожалуйста, попробуйте снова.")
+
+    def create_account(self):
+        self.manager.current = 'order_screen'
+
+
+class OrderScreen(Screen):
+    def order_pizza(self):
+        app = App.get_running_app()
+        customer_id = 1
+        pizza_id = 2
+
+        if app.username and app.password:
+            add_order(customer_id, pizza_id, app.username, app.password)
+            print("Заказ добавлен!")
+        else:
+            print("Пожалуйста, войдите в систему!")        
+
+class LoadingScreen(Screen):
+    def on_enter(self):
+        Clock.schedule_once(self.switch_to_main, 3) 
+
+    def switch_to_main(self, screen_name):
+        self.manager.current = screen_name
+
+
 class PizzaApp(App):
     username = ''
     password = ''
-
+    connection = None  
 
     def build(self):
-       
+        Window.size = (400, 800)
+        Window.resizable = False
         return Builder.load_file('structure/mobile_views.kv')
 
-    def on_start(self):
-        try:
-            Window.size = (400, 800)
-            Window.resizable = False
-            self.screen_manager = self.root
-        except AttributeError:
-            print("Error: Could not find ScreenManager")
-
-
-    def login(self):
-        
-        self.username = self.root.ids.username_input.text
-        self.password = self.root.ids.password_input.text
-
-        try:
-            connection = connect_to_db()
-            customer_repo : CustomerInterface = CustomerRepo(connection)
-            customer_repo.create_user(self.username, "",1, "08.12.2004", 2, 1)
-            connection.close() 
-            
-            self.screen_manager.current = 'order_screen'
-        except Exception as e:
-            print(f"Ошибка входа: {e}")
-            print("Неверное имя пользователя или пароль. Пожалуйста, попробуйте снова.")
-
-    def order_pizza(self):
-        customer_id = 1
-        pizza_id = 2
-        
-        # Используем сохранённые имя пользователя и пароль для добавления заказа
-        if self.username and self.password:
-            add_order(customer_id, pizza_id, self.username, self.password)
-            print("Заказ добавлен!")
-        else:
-            print("Пожалуйста, войдите в систему!")
+    def on_stop(self):
+        if self.connection:
+            self.connection.close()
 
 if __name__ == '__main__':
     PizzaApp().run()
